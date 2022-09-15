c973e9db0e465b2c486b65f7c589a7d85d918e6a
pupil <pupil@ivkhk.ee>
2022-09-15 10:30:15 +0300

    Added ...

--- src/jktv21arraynew/JKTV21ArrayNew.java
+++ src/jktv21arraynew/JKTV21ArrayNew.java
@@ -11,32 +11,32 @@ public class JKTV21ArrayNew {
 
     public static void main(String[] args) {
         //Example 1
-//        int[] numbers = new int[10];
-//        Random rnd = new Random();
-//        for (int i = 0; i < numbers.length; i++){
-//            numbers[i] = rnd.nextInt(100); //random numbers 0..99
-//            System.out.printf("%5d", numbers[i]);
-//        }
-//        System.out.println("");
-//        int max = 0;
-//        for (int i = 0; i < numbers.length; i++) {
-//            if (numbers[i] > max){
-//                max = numbers[i];
-//            }
-//        }
-//    System.out.println("Max number is "+ max);
-//    
-//    //example 2
-//        int[] numbers1={5,6,1,7,2};
-//        int[] numbers1Copy1=numbers1;
-//        numbers1Copy1[2]=10;
-//        System.out.printf("numbers1[2]=%d, numbers1Copy1[2]=%d%n", numbers1[2], numbers1Copy1[2]);
-//        
-//        int[] numbers1Copy2 = new int[numbers1.length];
-//        System.arraycopy(numbers1, 0, numbers1Copy2, 0, numbers1.length);
-//        numbers1Copy2[2]=100;
-//        System.out.printf("numbers1[2]=%d, numbers1Copy2[2]=%d%n", numbers1[2], numbers1Copy2[2]);
-//        
+        int[] numbers = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = rnd.nextInt(100); //random numbers 0..99
+            System.out.printf("%5d", numbers[i]);
+        }
+        System.out.println("");
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+    System.out.println("Max number is "+ max);
+    
+    //example 2
+        int[] numbers1={5,6,1,7,2};
+        int[] numbers1Copy1=numbers1;
+        numbers1Copy1[2]=10;
+        System.out.printf("numbers1[2]=%d, numbers1Copy1[2]=%d%n", numbers1[2], numbers1Copy1[2]);
+        
+        int[] numbers1Copy2 = new int[numbers1.length];
+        System.arraycopy(numbers1, 0, numbers1Copy2, 0, numbers1.length);
+        numbers1Copy2[2]=100;
+        System.out.printf("numbers1[2]=%d, numbers1Copy2[2]=%d%n", numbers1[2], numbers1Copy2[2]);
+        
     //example 3 
     int[] num = new int[10];
     Arrays.fill(num, 5);//fill(int[] a, int val)
